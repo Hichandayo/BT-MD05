d05bcd1eac0487041152e4dd7be2c822a64fb840
Hichandayo <nguyenhienthe1120@gmail.com>
2024-10-14 16:03:58 +0700

    up

---
+++ src/test/java/ra/md05/Md05ApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.md05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
